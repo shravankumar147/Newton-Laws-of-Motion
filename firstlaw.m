128a56e1c49fe1b21246f32c316ad2911b81a60b
shravankumar147 <shravankumar147@gmail.com>
2015-01-12 13:46:14 +0530

    help is added to the function

--- firstlaw.m
+++ firstlaw.m
@@ -1,6 +1,11 @@
-function [ v ] = firstlaw( ~,~,~ )
+function [ v ] = firstlaw( varargin )
 %Newton first law of motion. Summary of this function goes here
 %   Detailed explanation goes here
+% firstlaw
+% u:4
+% a:5
+% t:6
+% Your speed after 6 sec is 34 m/s
 u = input('u:');
 a = input('a:');
 t = input('t:');
